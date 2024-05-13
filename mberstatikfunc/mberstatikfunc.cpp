f6e83d8c675bcaf6abd8c52766ec41c6bfae6577
FarhatAsharfillah <farhatasharfillah03@gmail.com>
2024-05-13 19:52:40 +0700

    commit 10 input class mahasiswa dengan private isi static int dan public int dan string serta void

---
+++ mberstatikfunc/mberstatikfunc.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class mahasiswa {
+private:
+	static int nim;
+public:
+	int id;
+	string namna;
+
+	void setID();
+	void printAll();
+};
