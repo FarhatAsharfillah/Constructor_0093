543d0f50070e0b930a1e224b568a813dc6347121
FarhatAsharfillah <farhatasharfillah03@gmail.com>
2024-05-13 19:56:27 +0700

    commit 11 input static void dan static int serta mahasiswa dengan string

--- mberstatikfunc/mberstatikfunc.cpp
+++ mberstatikfunc/mberstatikfunc.cpp
@@ -11,4 +11,9 @@ public:
 
 	void setID();
 	void printAll();
+
+	static void setNim(int pNim) { nim = pNim; /*definisi function*/ }
+	static int getNim() { return nim; /*definisi function*/ }
+
+	mahasiswa(string pnama) :nama(pnama) { setID(); }
 };
