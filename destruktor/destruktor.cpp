ca36764036fb69a1ebc34b79482f29e416557795
FarhatAsharfillah <farhatasharfillah03@gmail.com>
2024-05-13 17:20:24 +0700

    commit 4 input class dengan private isi int dan public isi int, angka, void

---
+++ destruktor/destruktor.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+class angka {
+private :
+	int* arr;
+	int panjang;
+public:
+	angka(int); //constructor
+	~angkka(); //destructor
+	void cetakData();
+	void isiData();
+};
